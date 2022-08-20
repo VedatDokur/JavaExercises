32a4ebda4c8cfc45f6626862d6dbd57173919f8e
VedatDokur <111390308+VedatDokur@users.noreply.github.com>
2022-08-20 14:13:20 +0200

    Exercise08

---
+++ Exercise08.java
@@ -0,0 +1,18 @@
+package JavaExercise;
+
+public class Exercise08 {
+
+	public static void main(String[] args) {
+		//Area and perimeter of a circle - Write a program that displays the area and perimeter of a circle that has a radius of 5.5 using the following formula.
+		int i= 2;
+		double radius= 5.5;
+		double pi= 3.14;
+		double Circle= i*radius*pi;
+		double Area=  radius*radius*pi;
+		
+		System.out.println("Circle= " +Circle + "\n" +"Area= " +Area);
+		
+
+	}
+
+}
