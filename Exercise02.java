a056e58879b5cc74a31f7b889a0251c6bd028e0d
VedatDokur <111390308+VedatDokur@users.noreply.github.com>
2022-08-20 14:11:49 +0200

    Exercise02

---
+++ Exercise02.java
@@ -0,0 +1,14 @@
+package JavaExercise;
+
+public class Exercise02 {
+	public static void main(String[] args) {
+		//Display five messages- Write a program that displays Welcome to Java five times.
+		System.out.print("" +
+				"Welcome to Java \n" +
+				"Welcome to Java \n" +
+				"Welcome to Java \n" +
+				"Welcome to Java \n" +
+				"Welcome to Java");
+	}
+
+}
