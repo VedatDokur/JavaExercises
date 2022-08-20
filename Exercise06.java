e031b5c65db2b16befd84665417467163c9279ca
VedatDokur <111390308+VedatDokur@users.noreply.github.com>
2022-08-20 14:12:56 +0200

    Exercise06

---
+++ Exercise06.java
@@ -0,0 +1,18 @@
+package JavaExercise;
+
+public class Exercise06 {
+		public static void main (String [] args) {
+			 //Summation of a series - Write a program that displays the result of 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9.
+			int number, total = 0;
+			for( number=1; number<=9; number++)
+			{
+				total += number;
+			}
+			
+			System.out.println("Total = " + total);	
+	}
+}
+	
+		
+			
+
