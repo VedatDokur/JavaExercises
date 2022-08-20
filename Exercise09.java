1e19d345cb54e025653775fba742993e48e05701
VedatDokur <111390308+VedatDokur@users.noreply.github.com>
2022-08-20 14:13:32 +0200

    Exercise09

---
+++ Exercise09.java
@@ -0,0 +1,15 @@
+package JavaExercise;
+
+public class Exercise09 {
+
+	public static void main(String[] args) {
+		//Area and perimeter of a rectangle -Write a program that displays the area and perimeter of a rectangle with the width of 4.5 and height of 7.9 using the following formula: area = width * height.
+		double Width= 4.5;
+		double Height= 7.9;
+		double Area= Width*Height;
+		double Circle= (Width+Height)*2;
+		
+		System.out.println("Area= " +Area + "\n" +"Circle= " +Circle);
+	}
+
+}
