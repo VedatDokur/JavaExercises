6abb7a8a8baf3f969a4a6d72707b55f48631788d
VedatDokur <111390308+VedatDokur@users.noreply.github.com>
2022-08-20 14:12:06 +0200

    Exercise03

---
+++ Exercise03.java
@@ -0,0 +1,11 @@
+package JavaExercise;
+
+public class Exercise03 {
+	//Display a pattern - Write a program that displays the following pattern.
+	public static void main(String[] args) {
+	System.out.println("  J    A  V     V  A");
+    System.out.println("  J   A A  V   V  A A");
+    System.out.println("J J  AAAAA  V V  AAAAA");
+    System.out.println("J J A     A  V  A     A");
+	}
+}
