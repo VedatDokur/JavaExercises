e9ef783a4a1b557aa8b8ae3a49c2eb44aa59942c
VedatDokur <111390308+VedatDokur@users.noreply.github.com>
2022-08-20 14:11:32 +0200

    Exercise01

---
+++ Exercise01.java
@@ -0,0 +1,16 @@
+package JavaExercise;
+
+public class Exercise01 {
+	//Main method - Java Entry Point
+	public static void main(String[] args) {
+		//Display three messages - Write a program that displays Welcome to Java, Welcome to Computer Science, and Programming is fun.
+		//Step 1: Display- Welcome to Java
+		System.out.println("Welcome to Java");
+		
+		//Step 2: Display - Welcome to Computer Science
+		System.out.println("Welcome to Computer Science");
+		
+		//Step 3: Display - Programming is Fun
+		System.out.println("Programming is Fun");
+	}
+}
