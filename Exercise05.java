00471e8cf7e6b078efa1d448684f5eef5fc30b36
VedatDokur <111390308+VedatDokur@users.noreply.github.com>
2022-08-20 14:12:41 +0200

    Exercise05

---
+++ Exercise05.java
@@ -0,0 +1,21 @@
+package JavaExercise;
+
+public class Exercise05 {
+	//Compute expressions - Write a program that displays the result of (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5)
+	
+	public static void main(String[] args) {
+		System.out.println("Answer= "+ ((9.5*4.5)-(2.5*3))/(45.5-3.5));
+		
+		/*System.out.println("a=9.5*4.5");
+		float a = (float) (9.5*4.5);
+		System.out.println(a);
+		System.out.println("b=2.5*3");
+		float b = (float) (2.5*3);
+		System.out.println(b);
+		System.out.println("C=45.5-3.5");
+		float c = (float) (45.5-3.5);
+		System.out.println(c);
+		
+		System.out.println("(a-b)/c = " + (a-b) /c );*/
+	}
+}
